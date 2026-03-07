af6bd6dc1722d93a50756e88239342d67ee06a3c
Bob-2006 <robin.ribaute@gmail.com>
2026-03-07 11:19:06 +0100

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+struct tree_node {
+  int data;
+  struct tree_node *parent;
+  struct tree_node *left;
+  struct tree_node *right;
+};
+
+typedef struct tree_node TreeNode;
+
+int main(void) {
+    TreeNode root = {
+      .data = 'F',
+      .left = &(TreeNode){
+        .data = 'B',
+        .left = &(TreeNode){ .data = 'A', .left = NULL, .right = NULL, },
+        .right = &(TreeNode){
+          .data = 'D',
+          .left = &(TreeNode){ .data = 'C', .left = NULL, .right = NULL, },
+          .right = &(TreeNode){ .data = 'E', .left = NULL, .right = NULL, },
+        },
+      },
+      .right = &(TreeNode){ 
+        .data = 'G',
+        .left = NULL,
+        .right = &(TreeNode){
+          .data = 'I',
+          .left = &(TreeNode){ .data = 'H', .left = NULL, .right = NULL, },
+          .right = NULL,
+        },
+      },
+    };
+
+    return 0;
+}
